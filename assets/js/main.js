b79e80cc076995f4ab0c56510b6a507299d6c01c
Vladys K <vldk0022@gmail.com>
2024-10-24 20:41:29 +0200

    Game start styles added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", function () {
   const startBtn = document.getElementById("start-btn");
   const board = document.getElementById("game-board");
+  const description = document.getElementById("game-description");
   
   const game = new Game(board);
 
   startBtn.addEventListener("click", function () {
     startBtn.style.display = "none";
+    description.style.display = "none";
     game.start();
   });
 });
